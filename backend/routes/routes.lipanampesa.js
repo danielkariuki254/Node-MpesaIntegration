26f3b8989a532471311f2f653842d452bf47ed8d
danielkariuki254 <dankariuki202@gmail.com>
2024-03-15 16:05:50 +0300

    first commit

---
+++ backend/routes/routes.lipanampesa.js
@@ -0,0 +1,19 @@
+import express from "express";
+const router = express.Router();
+import {
+  initiateSTKPush,
+  stkPushCallback,
+  confirmPayment,
+} from "../controllers/controllers.lipanampesa.js";
+
+import { accessToken } from "../middlewares/middlewares.generateAccessToken.js";
+
+// console.log("Token Access", accessToken);
+
+router.route("/stkPush").post(accessToken, initiateSTKPush);
+router.route("/stkPushCallback/:Order_ID").post(stkPushCallback);
+router
+  .route("/confirmPayment/:CheckoutRequestID")
+  .post(accessToken, confirmPayment);
+
+export default router;
